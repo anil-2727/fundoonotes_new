4dc10996e6ec901d35dbf7830d76c14c20d8f616
anil-2727 <bali39959@gmail.com>
2022-01-02 16:32:58 +0530

    ADDED:added notes

--- src/components/dashboard/noteCard.js
+++ src/components/dashboard/noteCard.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 import {useNavigation} from '@react-navigation/native';
 
 
-export const NoteCard = (props) => {
+const NoteCard = (props) => {
 
 
     return (
